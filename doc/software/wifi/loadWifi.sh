cc41f04adfac668a25c240094617c2b7f45da0c5
Gregory Fromain <gregoryfromain@gmail.com>
2012-11-12 18:24:59 +0100

    maj installeur wifi 2

--- doc/software/wifi/loadWifi.sh
+++ doc/software/wifi/loadWifi.sh
@@ -1,7 +1,7 @@
 ﻿#!/bin/bash
 
 #Copie des fichiers de configuration du wifi
-cp --force ~/Baba-Robot/doc/software/wifi/wpa_supplicant.conf /etc/wpa_supplicant.conf
-cp --force ~/Baba-Robot/doc/software/wifi/interfaces /etc/network/interfaces
+sudo cp --force ~/Baba-Robot/doc/software/wifi/wpa_supplicant.conf /etc/wpa_supplicant.conf
+sudo cp --force ~/Baba-Robot/doc/software/wifi/interfaces /etc/network/interfaces
 #Activation du wifi.
 sudo ifup wlan0
